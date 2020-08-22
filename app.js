105f9729bf4b99b97dc00498e756a270bdc6490a
Allen <allenvs@mail.ru>
2020-08-22 15:22:01 +0300

    added solution

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
+function flatten(array) {
+    return(array.reduce((a, b) => a.concat(b)));
+};
+console.log(flatten(arrays));
 // → [1, 2, 3, 4, 5, 6]
